869253a9f26f3b339cdafbc485d23657c22f0d2b
akashak0823 <141407705+akashak0823@users.noreply.github.com>
2025-12-12 18:35:16 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['https://artibots-id-form.onrender.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true, // set to true only if you send cookies/auth from frontend
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Listening on ${port}`);
 }
 bootstrap();
